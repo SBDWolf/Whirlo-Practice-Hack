1e27e67c7987312dd76ff6ca2e68542afb49e692
SBDWolf <90091268+SBDWolf@users.noreply.github.com>
2023-10-15 06:03:49 +0200

    clear left side hud

--- src/edits.asm
+++ src/edits.asm
@@ -3,3 +3,17 @@
 ; remove game over on losing all lives
 org $00cee7
     bra $0b
+
+; don't display lives and fragments count (originally updates every frame)
+org $008304
+    nop #34
+
+; replace various pre-existing assets in the HUD with empty space
+org $1c8a41
+    db $40,$20,$40,$20
+org $1c8a47
+    db $40,$07,$20,$40,$20
+org $1c8a59
+    db $40,$20,$40,$20,$40
+org $1c8a60
+    db $40,$47,$20,$40,$20
