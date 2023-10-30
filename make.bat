77c872f0eaaa50dda9eeb2d6ed20a1548e887bf1
SBDWolf <90091268+SBDWolf@users.noreply.github.com>
2023-10-30 14:57:04 +0100

    don't adjust checksum (and version bump)

--- make.bat
+++ make.bat
@@ -1,8 +1,8 @@
-set rom_version=1.0
+set rom_version=1.0.1
 set rom_name=whirlo_prac_v%rom_version%.sfc
 
 IF NOT EXIST rom_output mkdir rom_output
 del /q rom_output
 copy rom_source\source.sfc rom_output\%rom_name%
 python make_characters_jp_bin.py
-tools\asar\asar.exe --no-title-check src/main.asm rom_output\%rom_name%
+tools\asar\asar.exe --no-title-check --fix-checksum=off src/main.asm rom_output\%rom_name%
